b9fd9754b68b9c75b41a67e76993c4533104b46d
Seth Hendrick <seth@shendrick.net>
2022-06-20 14:56:15 -0400

    Initial commit of HVCC project.

---
+++ src/HvccClock/Program.cs
@@ -0,0 +1,10 @@
+using HvccClock;
+
+IHost host = Host.CreateDefaultBuilder( args )
+    .ConfigureServices( services =>
+     {
+         services.AddHostedService<Worker>();
+     } )
+    .Build();
+
+await host.RunAsync();
