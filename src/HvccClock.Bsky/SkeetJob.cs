d5567fdbcbb08296312684cc3f1d39873106a4c8
Seth Hendrick <seth@shendrick.net>
2024-11-27 16:07:52 -0500

    Update bluesky client version

--- src/HvccClock.Bsky/SkeetJob.cs
+++ src/HvccClock.Bsky/SkeetJob.cs
@@ -35,9 +35,12 @@ namespace HvccClock.Bsky
         {
             var microsoftLogger = new SerilogLoggerFactory( log );
             this.client = new BlueskyClient(
+                new BlueskyHttpClientFactory(),
                 hvccConfig.BlueSkyUser,
                 hvccConfig.BlueSkyPassword,
+                new string[] { "en", "en-US" },
                 true,
+                new Uri( "https://at.shendrick.net" ),
                 microsoftLogger.CreateLogger<BlueskyClient>()
             );
         }
